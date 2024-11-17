cc590a9881eaf6c6c85c26c8568b1f0d534690d6
8anooceu <jaocarrilhooo2903@gmail.com>
2024-11-17 12:35:52 -0300

    Update 404.js

--- 404.js
+++ 404.js
@@ -1,5 +1,5 @@
 // Array com os nomes dos GIFs
-const gifs = ["erro1.gif", "erro2.gif", "erro3.gif"];
+const gifs = ["erro1.gif.gif", "erro2.gif.gif", "erro3.gif.gif"];
 
 // Seleciona aleatoriamente um GIF
 const randomGif = gifs[Math.floor(Math.random() * gifs.length)];
